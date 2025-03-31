41e8685d81936bd0791828b7d7109cc035f2f194
Akansha <akanshachaudhary999@gmail.com>
2025-03-31 14:13:10 +0200

    Added terraform files

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 provider "aws" {
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = var.aws_region
 }
 
 terraform {
   backend "s3" {
-    bucket = "deletelaters3backend"
-    key    = "workspace.statefile"
-    region = "us-east-1"
+    bucket         = "deletelaters3backend"
+    key            = "workspace.statefile"
+    region         = "us-east-1"
   }
 }
 
